7a84a13c14e7606096280eb71f94061c283d824b
Ang Xu <angxu1234@gmail.com>
2019-11-22 16:37:10 -0600

    through part 5

--- src/index.js
+++ src/index.js
@@ -1,72 +1,39 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  },
-  {
-    id: "1",
-    url: "a",
-    description: "b"
-  }
-];
-
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => {
-      const link = links.find(el => el.id === args.id);
-      return link;
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
     }
   },
-
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-
-    updateLink: (parent, args) => {
-      let { id, url, description } = args;
-      const link = links.find(el => el.id === id);
-      if (url) link.url = url;
-      if (description) link.description = description;
-      return link;
-
-      // let { id, url, description } = args;
-      // let link = links.find(el => el.id === id);
-      // let index = links.findIndex(el => el.id === id);
-      // if (url) link[url] = url;
-      // if (description) link[description] = description;
-      // links[index] = link;
-      // return link;
-    },
-
-    deleteLink: (parent, args) => {
-      let { id } = args;
-      if (id) links = links.filter(el => el.id !== id);
-      return links;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     }
   }
-
-  // Link: {
-  // 	id: parent => parent.id,
-  // 	description: parent => parent.description,
-  // 	url: parent => parent.url
-  // }
 };
+
+// updateLink: (parent, args) => {
+//   let { id, url, description } = args;
+//   const link = links.find(el => el.id === id);
+//   if (url) link.url = url;
+//   if (description) link.description = description;
+//   return link;
+
+// deleteLink: (parent, args) => {
+//   let { id } = args;
+//   if (id) links = links.filter(el => el.id !== id);
+//   return links;
+// }
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
